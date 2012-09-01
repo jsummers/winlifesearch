b799ae9858a22665ccbc21039e471c2cc5872734
Jason Summers <jason1@pobox.com>
2012-09-01 17:01:48 -0400

    Modernize the style of common controls and dialog boxes

--- source/wls.c
+++ source/wls.c
@@ -3,6 +3,25 @@
 // By Jason Summers
 //
 
+// Request a newer version of comctl32.dll.
+#ifdef _WIN64
+#pragma comment(linker, \
+	"\"/manifestdependency:type='Win32' "\
+	"name='Microsoft.Windows.Common-Controls' "\
+	"version='6.0.0.0' "\
+	"processorArchitecture='amd64' "\
+	"publicKeyToken='6595b64144ccf1df' "\
+	"language='*'\"")
+#else
+#pragma comment(linker, \
+	"\"/manifestdependency:type='Win32' "\
+	"name='Microsoft.Windows.Common-Controls' "\
+	"version='6.0.0.0' "\
+	"processorArchitecture='X86' "\
+	"publicKeyToken='6595b64144ccf1df' "\
+	"language='*'\"")
+#endif
+
 #include "wls-config.h"
 #define _CRT_SECURE_NO_WARNINGS
 #include <windows.h>
